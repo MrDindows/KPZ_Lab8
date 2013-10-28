e8a26e16f25b2603ce6ce873ea211e1ea7941d8f
Zhenya <ZZhenya94@mail.ru>
2013-10-28 11:13:04 +0200

    fi

--- src/Sample/Employee.java
+++ src/Sample/Employee.java
@@ -1,15 +1,7 @@
 package Sample;
 
-import javax.lang.model.element.Name;
 import java.util.Date;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Администратор
- * Date: 22.10.13
- * Time: 23:44
- * To change this template use File | Settings | File Templates.
- */
 public class Employee {
     public String Name;
     public Date BirthdayDate;
